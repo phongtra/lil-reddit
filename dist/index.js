9ae5cd91cfe123555b465a7f835a986454fdc1a0
Phong Tran <aphtr001@edu.xamk.fi>
2020-09-08 12:26:02 +0300

    setup apollo server

--- dist/index.js
+++ dist/index.js
@@ -13,13 +13,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const type_graphql_1 = require("type-graphql");
+const Hello_1 = require("./resolvers/Hello");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const posts = yield orm.em.find(Post_1.Post, {});
-    console.log(posts);
+    const app = express_1.default();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({ resolvers: [Hello_1.HelloResolver], validate: false })
+    });
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log('Listening on port 4000');
+    });
 });
 main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import mikroConfig from './mikro-orm.config';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/Hello';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  //   const post = orm.em.create(Post, { title: 'my first page' });
-  //   await orm.em.persistAndFlush(post);
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [HelloResolver], validate: false })
+  });
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log('Listening on port 4000');
+  });
 };
 
 main();
