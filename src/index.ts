1475ec5c90b1a6bea8c42a14677c841a6f52e4d0
Phong Tran <aphtr001@edu.xamk.fi>
2020-09-08 18:06:34 +0300

    add session and redis

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,43 @@ import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/Hello';
 import { PostResolver } from './resolvers/Post';
 import { UserResolver } from './resolvers/User';
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { MyContext } from './types';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365, //1 year
+        httpOnly: true,
+        sameSite: 'lax', //csrf
+        secure: __prod__ //cookie only works in https
+      },
+      secret: 'sadjkhsajhdksahdkasjhdakdhak',
+      resave: false
+    })
+  );
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
-    context: () => ({ em: orm.em })
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
   });
   apolloServer.applyMiddleware({ app });
   app.listen(4000, () => {
